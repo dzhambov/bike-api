0e365090267d5a119604df7fa7bf361d2e6a36ae
Student <example@example.com>
2020-02-12 11:43:03 -0800

    update ui

--- src/bike-interface.js
+++ src/bike-interface.js
@@ -1,16 +1,21 @@
+import { BikeService } from './../src/bike-service.js';
+
+
 $(document).ready(function() {
   $('#bikeLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
+    
 
     (async () => {
-      let response = await fetch('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen');
-      let jsonifiedResponse = await response.json();
-      this.getElements(jsonifiedResponse);
+      let bikeService = new BikeService();
+      const response = await bikeService.getBikeByCity(city)
+      getElements(response);
     })();
 
-    const getElements = function(response) {
+    function getElements(response) {
       $('#showManufacturer').text('The brand of bikes stolen in ${city} are the following:${response.bike.manufacturer_name}');
+      $('#showColor').text('The color of bikes stolen in ${city} are the following:${response.bike.frame_colors}');
     }
   });
 });
--- src/main.js
+++ src/main.js
@@ -2,3 +2,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { BikeService } from './../src/bike-service.js';
+
+
+$(document).ready(function() {
+  $('#bikeLocation').submit(function() {
+    event.preventDefault();
+    const city = $('#location').val();
+    $('#location').val("");
+    
+    
+
+    (async () => {
+      let bikeService = new BikeService();
+      const response = await bikeService.getBikeByCity(city)
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      console.log(response);
+      $('#showManufacturer').text(`The brand of bikes stolen in ${city} are the following:${response.bikes[0].manufacturer_name}`);
+      $('#showColor').text(`The color of bikes stolen in ${city} are the following:${response.bike.frame_colors}`);
+    }
+  });
+});
